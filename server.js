87109e20bd3207776ae777055ff97710eecd382d
Shad Rais <shadrais321@outlook.com>
2020-08-28 06:00:25 +0530

    Added test database

--- server.js
+++ server.js
@@ -2,8 +2,33 @@ const express = require('express');
 
 const app = express();
 
+const database = {
+    user:[
+        {
+            id:'123',
+            name:'john',
+            email:'john@gmail.com',
+            password:'1234',
+            entries:0,
+            joined: new Date()
+        },
+        {
+            id:'124',
+            name:'shad',
+            email:'shad@gmail.com',
+            password:'5678',
+            entries:0,
+            joined: new Date()
+        }
+    ]
+}
+
 app.get('/' , (req, res) => {
     res.send('this is working')
+})
+
+app.post('/signin', (req,res) =>{
+    res.json('signing');
 })
 
 app.listen(3000, () => {
