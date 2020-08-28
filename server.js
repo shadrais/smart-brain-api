fc0a70934399d25205c307340e2f29e0ee98cb3e
Shad Rais <shadrais321@outlook.com>
2020-08-28 16:46:04 +0530

    added signin and register backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const database = {
     user:[
@@ -24,11 +25,31 @@ const database = {
 }
 
 app.get('/' , (req, res) => {
-    res.send('this is working')
+    res.send(database.user);
 })
 
 app.post('/signin', (req,res) =>{
-    res.json('signing');
+    if(req.body.email===database.user[0].email && req.body.password===database.user[0].password)
+    {
+        res.json('success');
+    }
+    else 
+    {
+        res.status(400).json('fail');
+    }
+})
+
+app.post('/register',(req,res)=>{
+    const {name,email,password} = req.body;
+    database.user.push({
+        id:'125',
+        name: name,
+        email: email,
+        password:password,
+        entries:0,
+        joined: new Date()
+    })
+    res.json(database.user[database.user.length-1]);
 })
 
 app.listen(3000, () => {
