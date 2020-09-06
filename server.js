784d6021b421f5488d5a658bd707c3bd492cb3d0
Shad Rais <shadrais321@outlook.com>
2020-09-07 01:56:42 +0530

    Sql server added

--- server.js
+++ server.js
@@ -1,87 +1,101 @@
 const express = require('express');
 const cors = require('cors')
 const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+const { response } = require('express');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'Shad1234',
+      database : 'smat-brain'
+    }
+  });
 
 const app = express();
 app.use(express.json())
 app.use(cors())
 
-const database = {
-    user:[
-        {
-            id:'123',
-            name:'john',
-            email:'john@gmail.com',
-            password:'1234',
-            entries:0,
-            joined: new Date()
-        },
-        {
-            id:'124',
-            name:'shad',
-            email:'shad@gmail.com',
-            password:'5678',
-            entries:0,
-            joined: new Date()
-        }
-    ]
-}
+
 
 app.get('/' , (req, res) => {
     res.send(database.user);
 })
 
 app.post('/signin', (req,res) => {
-    if(req.body.email===database.user[0].email && req.body.password===database.user[0].password)
-    {
-        res.json('success');
-    }
-    else 
-    {
-        res.status(400).json('error logging in');
-    }
+    db.select('email','hash').from('login')
+    .where('email','=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password , data[0].hash);
+        if(isValid)
+        {
+            return db.select('*').from('users')
+            .where('email' , '=' , req.body.email)
+            .then(user =>{
+                res.json(user[0])
+            }).catch(err => res.status(400).json("unable to get user"))
+        }
+        else 
+        {
+            res.status(400).json("wrong credentials")
+        }
+    }).catch(err => res.status(400).json("wrong credentials"))
 })
 
 app.post('/register',(req,res)=>{
     const {name,email,password} = req.body;
-    database.user.push({
-        id:'125',
-        name: name,
-        email: email,
-        password:password,
-        entries:0,
-        joined: new Date()
-    })
-    res.json(database.user[database.user.length-1]);
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx =>{
+        trx.insert({
+            hash:hash,
+            email:email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail =>{
+            return trx('users')
+            .returning('*')
+            .insert({
+             email:loginEmail[0],
+             name:name,
+             joined:new Date()
+        })
+        .then(user => {
+            res.json(user[0]);
+        })
+    }).then(trx.commit)
+    .catch(trx.rollback)
+    }).catch(err => res.status(400).json("unable to register"))
 })
 
 app.get('/profile/:id',(req,res)=>{
     const {id} = req.params;
-    let found = false;
-    database.user.forEach(user => {
-        if(user.id===id) {
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if(user.length)
+        {
+            res.json(user[0])
         }
-    })
-    if(!found){
-        res.status(400).json('user not found');
-    }
-})
+        else
+        {
+            res.status(400).json("Not found")
+        }
+
+    }).catch(err => res.json("Error getting user"))
+}
+    )
 
 app.put('/image',(req,res)=>{
     const {id} = req.body;
-    let found = false;
-    database.user.forEach(user => {
-        if(user.id===id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    db('users').where('id','=',id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries)
     })
-    if(!found){
-        res.status(400).json('user not found');
-    }
+    .catch(err => res.status(400).json("unable to get count"))
 })
 
 app.listen(3001, () => {
