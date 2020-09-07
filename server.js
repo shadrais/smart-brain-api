eff0c8e647a4036a638591ac83c1dd8183970468
Shad Rais <shadrais321@outlook.com>
2020-09-08 00:22:35 +0530

    heroku fix root

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(cors())
 
 
 app.get('/', (req, res) => {
+    res.send('it is working')
 
 })
 
@@ -41,6 +42,6 @@ app.put('/image',(req,res)=>{image.handleImage(req,res,db   )})
 
 app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
